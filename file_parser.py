136f49a99ebf38ee6cd7719a98e6f271f0cc572f
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-05-06 14:42:20 +0300

    UPD

--- file_parser.py
+++ file_parser.py
@@ -1,49 +1,47 @@
 import sys
 from pathlib import Path
-from tkinter import ARC
 
 JPEG_IMAGES = []
 JPG_IMAGES = []
 PNG_IMAGES = []
 SVG_IMAGES = []
-MP3_AUDIO = []
-OGG_AUDIO = []
-WAV_AUDIO = []
-AMR_AUDIO = []
 AVI_VIDEO = []
 MP4_VIDEO = []
 MOV_VIDEO = []
 MKV_VIDEO = []
-DOC_DOCUMENT = []
-DOCX_DOCUMENT = []
-TXT_DOCUMENT = []
-PDF_DOCUMENT = []
-XLSX_DOCUMENT = []
-PPTX_DOCUMENT = []
-
-
-OTHER = []
+DOC_DOCS = []
+DOCX_DOCS = []
+TXT_DOCS = []
+PDF_DOCS = []
+XLSX_DOCS = []
+PPTX_DOCS = []
+MP3_AUDIO = []
+OGG_AUDIO = []
+WAV_AUDIO = []
+AMR_AUDIO = []
 ARCHIVES = []
+OTHER = []
+
 
 REGISTER_EXTENSIONS = {
     'JPEG': JPEG_IMAGES,
-    'PNG': PNG_IMAGES,
     'JPG': JPG_IMAGES,
+    'PNG': PNG_IMAGES,
     'SVG': SVG_IMAGES,
-    'MP3': MP3_AUDIO,
-    'OGG': OGG_AUDIO,
-    'WAV': WAV_AUDIO,
-    'AMR': AMR_AUDIO,
     'AVI': AVI_VIDEO,
     'MP4': MP4_VIDEO,
     'MOV': MOV_VIDEO,
     'MKV': MKV_VIDEO,
-    'DOC': DOC_DOCUMENT,
-    'DOCX': DOCX_DOCUMENT,
-    'TXT': TXT_DOCUMENT,
-    'PDF': PDF_DOCUMENT,
-    'XLSX': XLSX_DOCUMENT,
-    'PPTX': PPTX_DOCUMENT,
+    'DOC': DOC_DOCS,
+    'DOCX': DOCX_DOCS,
+    'TXT': TXT_DOCS,
+    'PDF': PDF_DOCS,
+    'XLSX': XLSX_DOCS,
+    'PPTX': PPTX_DOCS,
+    'MP3': MP3_AUDIO,
+    'OGG': OGG_AUDIO,
+    'WAV': WAV_AUDIO,
+    'AMR': AMR_AUDIO,
     'ZIP': ARCHIVES,
     'GZ': ARCHIVES,
     'TAR': ARCHIVES
@@ -55,30 +53,38 @@ UNKNOWN = set()
 
 
 def get_extension(filename: str) -> str:
+    # превращаем расширение файла в название паки jpg -> JPG
     return Path(filename).suffix[1:].upper()
 
 
 def scan(folder: Path) -> None:
     for item in folder.iterdir():
+        # если это папка, то добавляем ее в список Folders и переходим к следующему элементу папки
         if item.is_dir():
-            if item.name not in ('archives', 'video', 'audio', 'documents', 'images', 'OTHER', 'UNKNOWN'):
+            # проверяем, чтоб папка не была той, в которую мы складываем уже файлы
+            if item.name not in ('images', 'video', 'audio', 'documents', 'archives', 'OTHER'):
                 FOLDERS.append(item)
+                # сканируем эту вложенную папку - рекурсия
                 scan(item)
+            # перейти к следующему эдементу в сканируемой папке
             continue
 
-        ext = get_extension(item.name)
-        fullname = folder / item.name
-        if not ext:
+        # Пошла работа с файлом
+
+        ext = get_extension(item.name)  # взять расширение
+        fullname = folder / item.name  # взять полный путь к файлу
+        if not ext:  # если у файла нет расширения, добавить к неизвестным
             OTHER.append(fullname)
         else:
             try:
+                # взять список куда положить полный путь к файлу
                 container = REGISTER_EXTENSIONS[ext]
                 EXTENSIONS.add(ext)
                 container.append(fullname)
             except KeyError:
+                # если мы не регистрировали расширение файла в Register_extension, то добавить в другое
                 UNKNOWN.add(ext)
                 OTHER.append(fullname)
-                
 
 
 if __name__ == '__main__':
@@ -86,27 +92,27 @@ if __name__ == '__main__':
     print(f'Start in folder {folder_for_scan}')
 
     scan(Path(folder_for_scan))
-    print(f'Images jpeg: {JPEG_IMAGES}')
-    print(f'Images jpg: {JPG_IMAGES}')
-    print(f'Images png: {PNG_IMAGES}')
-    print(f'Images svg: {SVG_IMAGES}')
-    print(f'Audio mp3: {MP3_AUDIO}')
-    print(f'Audio ogg: {OGG_AUDIO}')
-    print(f'Audio wav: {WAV_AUDIO}')
-    print(f'Audio amr: {AMR_AUDIO}')
-    print(f'Video avi: {AVI_VIDEO}')
-    print(f'Video mp4: {MP4_VIDEO}')
-    print(f'Video mov: {MOV_VIDEO}')
-    print(f'Video mkv: {MKV_VIDEO}')
-    print(f'Document doc: {DOC_DOCUMENT}')
-    print(f'Document docx: {DOCX_DOCUMENT}')
-    print(f'Document txt: {TXT_DOCUMENT}')
-    print(f'Document pdf: {PDF_DOCUMENT}')
-    print(f'Document xlsx: {XLSX_DOCUMENT}')
-    print(f'Document pptx: {PPTX_DOCUMENT}')
+    print(f'Images JPEG: {JPEG_IMAGES}')
+    print(f'Images PNG: {PNG_IMAGES}')
+    print(f'Images JPG: {JPG_IMAGES}')
+    print(f'Images SVG: {SVG_IMAGES}')
+    print(f'Video AVI: {AVI_VIDEO}')
+    print(f'Video MP4: {MP4_VIDEO}')
+    print(f'Video MOV: {MOV_VIDEO}')
+    print(f'Video MKV: {MKV_VIDEO}')
+    print(f'Documents DOC: {DOC_DOCS}')
+    print(f'Documents DOCX: {DOCX_DOCS}')
+    print(f'Documents TXT: {TXT_DOCS}')
+    print(f'Documents PDF: {PDF_DOCS}')
+    print(f'Documents XLSX: {XLSX_DOCS}')
+    print(f'Documents PPTX: {PPTX_DOCS}')
+    print(f'Audio MP3: {MP3_AUDIO}')
+    print(f'Audio OGG: {OGG_AUDIO}')
+    print(f'Audio WAV: {WAV_AUDIO}')
+    print(f'Audio AMR: {AMR_AUDIO}')
     print(f'Archives: {ARCHIVES}')
 
-    print(f'Types of files in folder: {EXTENSIONS}')
-    print(f'Unknown files of types: {UNKNOWN}')
+    print(f'There are following types of files: {EXTENSIONS}')
+    print(f'Unknown file types: {UNKNOWN}')
 
-    print(FOLDERS[::-1])
+    print({FOLDERS})
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import shutil
 import sys
-import file_parser as parser
 from normalize import normalize
+import file_parser as parser
 
 
 def handle_media(filename: Path, target_folder: Path):
@@ -15,16 +15,16 @@ def handle_other(filename: Path, target_folder: Path):
     filename.replace(target_folder / normalize(filename.name))
 
 
-def handle_unknown(filename: Path, target_folder: Path):
-    target_folder.mkdir(exist_ok=True, parents=True)
-    filename.replace(target_folder / normalize(filename.name))
-
-
 def handle_archive(filename: Path, target_folder: Path):
+    # создаем папку для архивов
     target_folder.mkdir(exist_ok=True, parents=True)
+    # создаем папку куда распаковываем архив
+    # берем суффикс у файла и убираем replpyace(filename.suffix,'')
     folder_for_file = target_folder / \
         normalize(filename.name.replace(filename.suffix, ''))
-    folder_for_file.mkdir(exist_ok=True, parents=True)
+
+    # создаем папку для архива с именем файла
+    folder_for_file. mkdir(exist_ok=True, parents=True)
     try:
         shutil.unpack_archive(str(filename.resolve()),
                               str(folder_for_file.resolve()))
@@ -39,7 +39,7 @@ def handle_folder(folder: Path):
     try:
         folder.rmdir()
     except OSError:
-        print(f'Не удалось удалить папку {folder}')
+        print(f' Не удалось удалить папку {folder}')
 
 
 def main(folder: Path):
@@ -53,14 +53,6 @@ def main(folder: Path):
         handle_media(file, folder / 'images' / 'PNG')
     for file in parser.SVG_IMAGES:
         handle_media(file, folder / 'images' / 'SVG')
-    for file in parser.MP3_AUDIO:
-        handle_media(file, folder / 'audio' / 'MP3')
-    for file in parser.OGG_AUDIO:
-        handle_media(file, folder / 'audio' / 'OGG')
-    for file in parser.WAV_AUDIO:
-        handle_media(file, folder / 'audio' / 'WAV')
-    for file in parser.AMR_AUDIO:
-        handle_media(file, folder / 'audio' / 'AMR')
     for file in parser.AVI_VIDEO:
         handle_media(file, folder / 'video' / 'AVI')
     for file in parser.MP4_VIDEO:
@@ -69,25 +61,33 @@ def main(folder: Path):
         handle_media(file, folder / 'video' / 'MOV')
     for file in parser.MKV_VIDEO:
         handle_media(file, folder / 'video' / 'MKV')
-    for file in parser.DOC_DOCUMENT:
-        handle_media(file, folder / 'document' / 'DOC')
-    for file in parser.DOCX_DOCUMENT:
-        handle_media(file, folder / 'document' / 'DOCX')
-    for file in parser.TXT_DOCUMENT:
-        handle_media(file, folder / 'document' / 'TXT')
-    for file in parser.PDF_DOCUMENT:
-        handle_media(file, folder / 'document' / 'PDF')
-    for file in parser.XLSX_DOCUMENT:
-        handle_media(file, folder / 'document' / 'XLSX')
-    for file in parser.PPTX_DOCUMENT:
-        handle_media(file, folder / 'document' / 'PPTX')
+    for file in parser.DOC_DOCS:
+        handle_media(file, folder / 'documents' / 'DOC')
+    for file in parser.DOCX_DOCS:
+        handle_media(file, folder / 'documents' / 'DOCX')
+    for file in parser.TXT_DOCS:
+        handle_media(file, folder / 'documents' / 'TXT')
+    for file in parser.PDF_DOCS:
+        handle_media(file, folder / 'documents' / 'PDF')
+    for file in parser.XLSX_DOCS:
+        handle_media(file, folder / 'documents' / 'XLSX')
+    for file in parser.PPTX_DOCS:
+        handle_media(file, folder / 'documents' / 'PPTX')
+    for file in parser.MP3_AUDIO:
+        handle_media(file, folder / 'audio' / 'MP3')
+    for file in parser.OGG_AUDIO:
+        handle_media(file, folder / 'audio' / 'OGG')
+    for file in parser.WAV_AUDIO:
+        handle_media(file, folder / 'audio' / 'WAV')
+    for file in parser.AMR_AUDIO:
+        handle_media(file, folder / 'audio' / 'AMR')
 
     for file in parser.OTHER:
         handle_other(file, folder / 'OTHER')
     for file in parser.ARCHIVES:
         handle_archive(file, folder / 'archives')
-    for file in parser.UNKNOWN:
-        handle_unknown(file, folder / 'UNKNOWN')
+
+    # выполняем реверс списка для того, чтоб все папки удалить
     for folder in parser.FOLDERS[::-1]:
         handle_folder(folder)
 
